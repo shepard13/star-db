688077d05e406028fcdcfdaa497e70bcbc8e2a0d
Andrey Yurets <yurets.andrey13@gmail.com>
2021-06-30 10:16:54 +0300

    Create swapi-service.js

---
+++ src/services/swapi-service.js
@@ -0,0 +1,34 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} recived ${res.status}`);
+    }
+    return await res.json();
+  }
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+  getPlanets(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
+  async getAllStarships() {
+    const res = await this.getResource(`/startships/`);
+    return res.results;
+  }
+  getStarship(id) {
+    return this.getResource(`/startship/${id}/`);
+  }
+}
+const swapi = new SwapiService();
+swapi.getPerson(3).then((people) => {
+  console.log(people.name);
+});
